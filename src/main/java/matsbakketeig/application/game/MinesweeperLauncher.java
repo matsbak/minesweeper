c849955ff6fd4da6440af8eba0e487ebe4b3c6b4
Mats Bakketeig <matsbak@stud.ntnu.no>
2024-01-06 00:55:29 +0100

    Edited line spacing in launcher class

--- src/main/java/matsbakketeig/application/game/MinesweeperLauncher.java
+++ src/main/java/matsbakketeig/application/game/MinesweeperLauncher.java
@@ -1,22 +1,23 @@
 package matsbakketeig.application.game;
 
 /**
- * The MinesweeperLauncher class represents the starting point of the Minesweeper application. The sole purpose of the class is
- * to run the <b>main</b> static method.
+ * The MinesweeperLauncher class represents the starting point of the Minesweeper application. The
+ * sole purpose of the class is to run the <b>main</b> static method.
  * 
  * @author  Mats Bakketeig
  * @version v1.0 (08.07.2023)
  */
 public class MinesweeperLauncher {
   /**
-   * Is the main starting point of the application. The operating system on the computer expects to find a publicly available
-   * method it can call without having to create an instance of a class first.
+   * Is the main starting point of the application. The operating system on the computer expects to
+   * find a publicly available method it can call without having to create an instance of a class
+   * first.
    * 
-   * <p>This method is standardized across programming languages and operating systems and must have the method signature given
-   * below.</p>
+   * <p>This method is standardized across programming languages and operating systems and must
+   * have the method signature given below.</p>
    * 
-   * @param args A fixed sized array of strings holding arguments provided from the command line during the startup of the
-   *             application.
+   * @param args A fixed sized array of strings holding arguments provided from the command line
+   *             during the startup of the application.
    */
   public static void main(String[] args) {
   }
